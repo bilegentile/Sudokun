b2625636743ffadd60c552caf3a575f7891b4f11
bilegentile <76962962+BileGentile@users.noreply.github.com>
2024-03-25 17:48:39 +0100

    Project creation
    -Added SudokuGrid as Model

---
+++ Sudokify/Sudokify/SudokifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokifyApp.swift
+//  Sudokify
+//
+//  Created by Beniamino Gentile on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SudokifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sudokify/Sudokify/SudokuGrid.swift
@@ -0,0 +1,13 @@
+//
+//  SudokuGrid.swift
+//  Sudokify
+//
+//  Created by Beniamino Gentile on 25/03/24.
+//
+
+import Foundation
+
+struct SudokuGrid: Codable {
+    let grid: [String]
+    let task: [String]
+}
